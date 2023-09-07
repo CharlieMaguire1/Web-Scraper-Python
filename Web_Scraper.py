fb82f09659f48654950080b6776690174249aa35
CharlieMaguire1 <144113514+CharlieMaguire1@users.noreply.github.com>
2023-09-07 09:27:11 +0100

    Add files via upload
    Signed-off-by: CharlieMaguire1 <144113514+CharlieMaguire1@users.noreply.github.com>

---
+++ Web_Scraper.py
@@ -0,0 +1,23 @@
+"""
+ This small project is about webscraping
+ And parsing XML sitemaps
+ 
+"""
+# Loading packages -for example urllib to fetch XML sitemaps
+import pandas as pd
+import urllib.request
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+  
+# url goes to the XML sitemap
+# BeautifulSoup lxml allows you to parse XML files
+def retrieve_sitemap(url):
+      resp = urllib.request.urlopen(url)
+      xml = BeautifulSoup(resp, 'lxml-xml', from_encoding=resp.info().get_param('charset'))
+      return xml
+      
+# Finding sitemap at the root(sitemap.xml) or using robots.txt      
+url = "https://dreamsfertilityclinic.com/local-sitemap.xml"
+xml = retrieve_sitemap(url)
+    
+xml    
